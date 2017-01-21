149ae5b64dc130d02b850c5ec4f8fc947ac4243d
Javier Luna molina <javierlunamolina@gmail.com>
2017-01-22 00:17:15 +0100

    Añadido widget y sistema de paginación

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import flash, redirect, render_template, url_for
+from flask import flash, redirect, render_template, url_for, current_app, request
 from flask_login import login_required, current_user
 from sqlalchemy.exc import IntegrityError
 
@@ -10,7 +10,13 @@ from ..models import Post, Tag, Comment, User
 
 @main.route('/')
 def frontpage():
-	return render_template("main/frontpage.html", posts=Post.query.all(), tags=Tag.query.all())
+	page = request.args.get('page', 1, type=int)
+	pagination = Post.query.order_by(Post.created_on.desc()).paginate(
+		page, per_page=current_app.config['POSTS_PER_PAGE'], error_out=False
+	)
+	return render_template("main/frontpage.html", posts=pagination.items, tags=Tag.query.all(), pagination=pagination,
+	                       prevpage=url_for('main.frontpage', page=pagination.prev_num),
+	                       nextpage=url_for('main.frontpage', page=pagination.next_num))
 
 
 @main.route('/post/<post_slug>', methods=['GET', 'POST'])
@@ -30,10 +36,16 @@ def post_detail(post_slug):
 
 @main.route('/tag/<tag_slug>')
 def tagged_posts(tag_slug):
+	page = request.args.get('page', 1, type=int)
 	tag = Tag.query.filter_by(slug=tag_slug).first_or_404()
-	return render_template("main/frontpage.html", posts=tag.posts.all(), tags=Tag.query.all(),
-	                       blog_title="Tagged as " + tag.name, blog_subtitle="",
-	                       title="Tagged as " + tag.name)
+	pagination = tag.posts.order_by(Post.created_on.desc()).paginate(
+		page, per_page=current_app.config['POSTS_PER_PAGE'], error_out=False
+	)
+	return render_template("main/frontpage.html", posts=pagination.items, tags=Tag.query.all(),
+	                       blog_title="Tagged as " + tag.name, blog_subtitle="", title="Tagged as " + tag.name,
+	                       pagination=pagination,
+	                       prevpage=url_for('main.tagged_posts', tag_slug=tag_slug, page=pagination.prev_num),
+	                       nextpage=url_for('main.tagged_posts', tag_slug=tag_slug, page=pagination.next_num))
 
 
 @main.route('/create/post', methods=['GET', 'POST'])
@@ -76,6 +88,7 @@ def create_post():
 	return render_template("main/postcreate.html", post_form=post_form, tag_form=tag_form, blog_title="Post something!",
 	                       blog_subtitle="Hope you're inspired")
 
+
 @main.route('/user')
 @login_required
 def my_user():
@@ -84,7 +97,13 @@ def my_user():
 
 @main.route('/user/<username>')
 def user_posts(username):
+	page = request.args.get('page', 1, type=int)
 	user = User.query.filter_by(username=username).first_or_404()
-	return render_template("main/frontpage.html", posts=user.posts.all(), tags=Tag.query.all(),
-	                       blog_title="Posts by " + user.username, blog_subtitle="", title="Posts by " + user.username)
-
+	pagination = user.posts.order_by(Post.created_on.desc()).paginate(
+		page, per_page=current_app.config['POSTS_PER_PAGE'], error_out=False
+	)
+	return render_template("main/frontpage.html", posts=pagination.items, tags=Tag.query.all(),
+	                       blog_title="Posts by " + user.username, blog_subtitle="", title="Posts by " + user.username,
+	                       pagination=pagination,
+	                       prevpage=url_for('main.user_posts', username=username, page=pagination.prev_num),
+	                       nextpage=url_for('main.user_posts', username=username, page=pagination.next_num))
--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'hunter02'
 	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 	SQLALCHEMY_TRACK_MODIFICATIONS = True
+	POSTS_PER_PAGE = 4
 	@staticmethod
 	def init_app(app):
 		pass
