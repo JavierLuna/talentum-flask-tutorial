0370dd3ec6d68c9812ee61a733da337d293be58a
Javier Luna molina <javierlunamolina@gmail.com>
2017-01-22 00:11:35 +0100

    Actualizadas vistas y añadidas un par más

--- app/main/views.py
+++ app/main/views.py
@@ -4,8 +4,8 @@ from sqlalchemy.exc import IntegrityError
 
 from app import db
 from . import main
-from .forms import PostCreationForm, TagCreationForm
-from ..models import Post, Tag
+from .forms import PostCreationForm, TagCreationForm, CommentCreationForm
+from ..models import Post, Tag, Comment, User
 
 
 @main.route('/')
@@ -13,10 +13,19 @@ def frontpage():
 	return render_template("main/frontpage.html", posts=Post.query.all(), tags=Tag.query.all())
 
 
-@main.route('/post/<post_slug>')
+@main.route('/post/<post_slug>', methods=['GET', 'POST'])
 def post_detail(post_slug):
 	post = Post.query.filter_by(slug=post_slug).first_or_404()
-	return render_template('main/postdetail.html', post=post, tags=Tag.query.all())
+	form = CommentCreationForm()
+	if form.validate_on_submit():
+		comment = Comment(body=form.content.data, user=current_user, post=post)
+		db.session.add(comment)
+		try:
+			db.session.commit()
+		except IntegrityError:
+			db.session.rollback()
+			flash("There was an error while posting your comment")
+	return render_template('main/postdetail.html', post=post, tags=Tag.query.all(), form=form)
 
 
 @main.route('/tag/<tag_slug>')
@@ -66,3 +75,16 @@ def create_post():
 			flash("The tag already existed")
 	return render_template("main/postcreate.html", post_form=post_form, tag_form=tag_form, blog_title="Post something!",
 	                       blog_subtitle="Hope you're inspired")
+
+@main.route('/user')
+@login_required
+def my_user():
+	return render_template('main/account_info.html', user=current_user)
+
+
+@main.route('/user/<username>')
+def user_posts(username):
+	user = User.query.filter_by(username=username).first_or_404()
+	return render_template("main/frontpage.html", posts=user.posts.all(), tags=Tag.query.all(),
+	                       blog_title="Posts by " + user.username, blog_subtitle="", title="Posts by " + user.username)
+
