9d844206ee73d91187c5f7fb2b0b54919336bd2a
Javier Luna molina <javierlunamolina@gmail.com>
2017-01-21 23:41:14 +0100

    Añadidas las primeras vistas de nuestro blog!! Usando Flask-Bootstrap y la blueprint “main”

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 from config import config
 
 
 db = SQLAlchemy()
+bootstrap = Bootstrap()
 
 def create_app(config_name='default'):
 	app = Flask(__name__, static_folder='./static')
@@ -12,6 +14,7 @@ def create_app(config_name='default'):
 
 	# Inicializar plugins aquí
 	db.init_app(app)
+	bootstrap.init_app(app)
 
 	# Registrar blueprints aquí
 	from .main import main as main_blueprint
--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,23 @@
+from flask import render_template
+
 from . import main
+from ..models import Post, Tag
+
 
 @main.route('/')
-def index():
-	return "Index"
+def frontpage():
+	return render_template("main/frontpage.html", posts=Post.query.all(), tags=Tag.query.all())
+
+
+@main.route('/post/<post_slug>')
+def post_detail(post_slug):
+	post = Post.query.filter_by(slug=post_slug).first_or_404()
+	return render_template('main/postdetail.html', post=post, tags=Tag.query.all())
+
+
+@main.route('/tag/<tag_slug>')
+def tagged_posts(tag_slug):
+	tag = Tag.query.filter_by(slug=tag_slug).first_or_404()
+	return render_template("main/frontpage.html", posts=tag.posts.all(), tags=Tag.query.all(),
+	                       blog_title="Tagged as " + tag.name, blog_subtitle="",
+	                       title="Tagged as " + tag.name)
